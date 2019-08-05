067c0bc5d4fc21ee5c944bb9d6523b4007c7422c
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-05 11:36:44 -0500

    constants, init, and partial render

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 /*----- constants -----*/ 
-let PLAYER {
-    "0": null;
-    "1": Red;
-    "-1": White;
-}
-
+let PLAYER = {
+    "0": null,
+    "1": "Black",
+    "-1": "White",
+};
+let PIECES = {
+    "1": "img/black_piece.png",
+    "1K": "img/black_king.png",
+    "-1": "img/white_piece.png",
+    "-1K": "img/white_kings.png",
+};
 
 /*----- app's state (variables) -----*/ 
 let board, turn, winner;
 
 
 /*----- cached element references -----*/ 
-let msgEl = getElementById('msg');
+let msgEl = document.getElementById('msg');
 
 
 /*----- event listeners -----*/ 
@@ -20,13 +25,27 @@ let msgEl = getElementById('msg');
 
 /*----- functions -----*/
 function init(){
-    board = {
+    board = [
+        [0, -1, 0, -1, 0, -1, 0, -1],
+        [-1, 0, -1, 0, -1, 0, -1, 0],
+        [0, -1, 0, -1, 0, -1, 0, -1],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+    ];
+    turn = 1;
+    winner = null;
+    // render();
+}
+
+function render(){
+    //render the board
+    board.forEach(function(colArr, colIdx){
+        colArr.forEach(function(cell, rowIdx){
+            let piece = document.getElementById(`r${rowIdx}c${colIdx}`);
+            piece.
+        })
+    });
 }
