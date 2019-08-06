48f38ca90540c0e2915a43d376cd2a038e92deb2
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-06 14:39:56 -0500

    refactor for classes and objects

--- js/script.js
+++ js/script.js
@@ -12,8 +12,37 @@ let PLAYER = {
         K: "img/white_king.png",
     },
 };
+
+class piece{
+    constructor(colorCode, rankCode){
+        this.colorCode = colorCode
+        this.rankCode = rankCode;
+        this.code = `${colorCode}${rankCode}`;
+        this.colorIdent = function(){
+            if(this.colorCode == 1){
+                return 'black';
+            }else{
+                return 'white';
+            };
+        };
+        this.rankIdent = function(){
+            if(this.rankCode == 'K'){
+                return 'king';
+            }else{
+                return 'piece';
+            };
+        };
+        this.color = this.colorIdent();
+        this.rank = this.rankIdent();
+        this.img = `img/${this.color}_${this.rank}.png`;
+
+    };
+};
+
 /*----- app's state (variables) -----*/ 
 let board, turn, winner, initSq, blackScore, whiteScore;
+let b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
+let w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12;
 
 
 /*----- cached element references -----*/ 
@@ -22,8 +51,7 @@ let btn = document.getElementById('btn');
 
 /*----- event listeners -----*/ 
 document.querySelectorAll('img').forEach(img => img.addEventListener('click', handlePiece));
-btn.addEventListener('click', function () {if(btn.textContent === 'Start'){start();}else{init();
-render();}});
+btn.addEventListener('click', function () {if(btn.textContent === 'Start'){start();}else{init();render();}});
 
 
 
@@ -50,15 +78,11 @@ function render(){
     //render the board
     board.forEach(function(rowArr, rowIdx){
         rowArr.forEach(function(cell, colIdx){
-            let checker = document.getElementById(`r${rowIdx}c${colIdx}`).querySelector('img');
-            if(cell === "1K" || cell === "-1K"){
-              let king = cell.slice(-1);
-              cell = cell.slice(0,-1);
-              checker.src = `${PLAYER[cell][king]}`;
-            } else if(cell == 1 || cell == -1){
-              checker.src = `${PLAYER[cell].P}`;
+            let checker = document.getElementById(`r${rowIdx}c${colIdx}`);
+            if(!cell == 0){
+                checker.innerHTML = `<img src='${cell.img}'>`;
             } else {
-              checker.src = "";
+                checker.innerHTML = '';
             };
         });
     });
@@ -66,19 +90,20 @@ function render(){
 };
 
 function start() {
+    b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = b10 = b11 = b12 = new piece('1','P');
+    w1 = w2 = w3 = w4 = w5 = w6 = w7 = w8 = w9 = w10 = w11 = w12 = new piece('-1','P');
     board = [
-        [0, "1K", 0, 1, 0, "1", 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1],
-        [0, 0, 0, 0, 0, "1K", 0, 0],
-        [0, 0, "-1K", 0, 0, 0, 0, 0],
-        [-1, 0, -1, 0, "-1", 0,-1, 0],
-        [0, -1, 0, -1, 0, -1, 0, -1],
-        [-1, 0, -1, 0, -1, 0, -1, 0],
+        [0, b1, 0, b2, 0, b3, 0, b4],
+        [b5, 0, b6, 0, b7, 0, b8, 0],
+        [0, b9, 0, b10, 0, b11, 0, b12],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [w1, 0, w1, 0, w3, 0, w4, 0],
+        [0, w5, 0, w6, 0, w7, 0, w8],
+        [w9, 0, w10, 0, w11, 0, w12, 0],
     ];
     render();
     btn.textContent = 'Give Up!';
-
 
 }
 
@@ -103,14 +128,12 @@ function handlePieceMove(iSq, fSq){
     let initColIdx = iSq.target.parentElement.id.charAt(3);
     let finRowIdx = fSq.target.id.charAt(1);
     let finColIdx = fSq.target.id.charAt(3);
+    let pieceMoved = board[initRowIdx][initColIdx];
 
-    if(board[initRowIdx][initColIdx] == 1 || board[initRowIdx][initColIdx] == -1 || board[initRowIdx][initColIdx] == 0){
+    if(!board[initRowIdx][initColIdx] == 0){
         board[initRowIdx][initColIdx] = 0;
-        board[finRowIdx][finColIdx] = turn;
-    } else if(board[initRowIdx][initColIdx] == "1K" || board[initRowIdx][initColIdx] == "-1K"){
-        board[initRowIdx][initColIdx] = 0;
-        board[finRowIdx][finColIdx] = `${turn}K`;
-    }
+        board[finRowIdx][finColIdx] = pieceMoved;
+    };
 
     render();
 };
